554de7c06e0f55f7192279a835aa7b4f1066a604
Agustin Luparini <agusluparini@gmail.com>
2023-12-13 15:48:12 -0300

    tailwind config fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
