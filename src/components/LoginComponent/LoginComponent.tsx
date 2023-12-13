ef7b2c3d3753d80962adfd2114d3f6e6865b2543
Agustin Luparini <agusluparini@gmail.com>
2023-12-13 17:10:12 -0300

    logo in login

--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react";
 import './LoginComponent.css'
 import {posting} from "../../services/API";
-
+import img from "../../assets/erp-icon.svg"
 
 
 export const LoginComponent = () =>{
@@ -28,8 +28,8 @@ export const LoginComponent = () =>{
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img
-                    className="mx-auto h-10 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    className="mx-auto h-20 w-auto"
+                    src={img}
                     alt="Your Company"
                 />
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
