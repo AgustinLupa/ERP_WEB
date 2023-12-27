808a1431f964095c60f482633538427908e9054f
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2023-12-27 16:36:57 -0300

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { LoginComponent } from './components/LoginComponent/LoginComponent'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { MenuComponent } from "./components/menu"
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginComponent/>} />
+        <Route path="home" element={<MenuComponent/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import {posting} from "../../services/API";
 import ReactLogo from "../../assets/erp-icon.svg";
+import {Link} from "react-router-dom";
 
 
 export const LoginComponent = () =>{
@@ -39,54 +40,57 @@ export const LoginComponent = () =>{
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" onSubmit={loginHandler} method="POST">
                     <div>
-                    <label htmlFor="account" className="block text-sm font-medium leading-6 text-gray-900">
-                        Email address
-                    </label>
-                    <div className="mt-2">
-                        <input
-                        id="account"
-                        name="account"
-                        type="account"
-                        onChange={(e) =>setFormData({...formData, username: e.target.value })}
-                        autoComplete="email"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                    </div>
-
-                    <div>
-                    <div className="flex items-center justify-between">
-                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Password
+                        <label htmlFor="account" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email address
                         </label>
-                        <div className="text-sm">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                            Forgot password?
-                        </a>
+                        <div className="mt-2">
+                            <input
+                            id="account"
+                            name="account"
+                            type="account"
+                            onChange={(e) =>setFormData({...formData, username: e.target.value })}
+                            autoComplete="email"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
                         </div>
                     </div>
-                    <div className="mt-2">
-                        <input
-                        id="password"
-                        name="password"
-                        type="password" 
-                        onChange={(e) =>
-                            setFormData({...formData, password : e.target.value})}
-                        autoComplete="current-password"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            Password
+                            </label>
+                            <div className="text-sm">
+                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                Forgot password?
+                            </a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            
+                            <input
+                            id="password"
+                            name="password"
+                            type="password" 
+                            onChange={(e) =>
+                                setFormData({...formData, password : e.target.value})}
+                            autoComplete="current-password"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                           
+                        </div>
                     </div>
 
                     <div>
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Sign in
-                    </button>
+                        <Link to="/home"> 
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
+                        </Link> 
                     </div>
                 </form>
 
