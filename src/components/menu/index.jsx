dddeb3c5a236805a9d371c5b9e9ee4392992035b
Maximiliano Scollo <Maximilianoscollo17@gmail.com>
2023-12-28 12:54:04 -0300

    Menu icon pre-fix

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,5 +1,7 @@
-
 import ReactLogo from "../../assets/erp-icon.svg";
+
+
+
 
 export const MenuComponent = () =>{
 
@@ -26,19 +28,19 @@ return(
                     <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Search</span>
+                    <span class="mx-5 ml-2 text-sm font-medium">Search</span>
                 </a>
                 <a class="flex items-center max-[550px]:justify-center w-full h-12 px-3 mt-2 text-gray-400 bg-indigo-700 text-indigo-100 rounded" href="#">
                     <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Insights</span>
+                    <span class="mx-3 ml-2 text-sm font-medium">Insights</span>
                 </a>
                 <a class="flex items-center max-[550px]:justify-center w-full h-12 px-3 mt-2 rounded hover:bg-indigo-700" href="#">
                     <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium min-w-72 ">Docs</span>
+                    <span class="mx-7 ml-2 text-sm font-medium">Docs</span>
                 </a>
             </div>
             <div class="flex flex-col items-center w-full mt-2 border-t border-gray-700">
@@ -46,19 +48,19 @@ return(
                     <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Products</span>
+                    <span class="mx-1 ml-2 text-sm font-medium">Products</span>
                 </a>
                 <a class="flex items-center max-[550px]:justify-center w-full h-12 px-3 mt-2 rounded hover:bg-indigo-700" href="#">
                     <svg class="w-6 h-6 stroke-current"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Settings</span>
+                    <span class="mx-2.5 ml-2 text-sm font-medium">Settings</span>
                 </a>
                 <a class="relative flex items-center max-[550px]:justify-center w-full h-12 px-3 mt-2 rounded hover:bg-indigo-700" href="#">
-                    <svg class="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg class="w- h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                     </svg>
-                    <span class="ml-2 text-sm font-medium">Messages</span>
+                    <span class="ml-2 text-sm font-medium ">Messages</span>
                     <span class="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
                 </a>
             </div>
